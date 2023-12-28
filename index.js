aad5f6faa7629e74355ac4dc770da57f4ce2af84
AshB <ashbasn@gmail.com>
2023-12-28 17:40:08 -0600

    Added console log

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ console.log(textIn);
 const textOut = `This is what we know about avocado: ${textIn}.\n Created on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written');
+console.log('Ready');
