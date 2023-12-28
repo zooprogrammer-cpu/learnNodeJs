d2a80c7513dec834b1d4cd2e37fd4a6d008048ea
AshB <ashbasn@gmail.com>
2023-12-28 17:36:53 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about avocado: ${textIn}.\n Created on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written');
